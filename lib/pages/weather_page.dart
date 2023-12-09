625792b49770be97cf106473f4c69c0f9ea182d3
Pranay7824 <pranaymaheriya899@gmail.com>
2023-12-09 00:03:24 -0500

    added back button in weather page

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -137,6 +137,12 @@ class _WeatherPageState extends State<WeatherPage> {
               'Wind Speed: $_windSpeed m/s',
               style: TextStyle(fontSize: 20),
             ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/home');
+              },
+              child: Text('Skip to Home'),
+            ),
           ],
         ),
       ),
